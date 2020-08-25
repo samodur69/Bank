5f025214309887ae373123b7506f5f7e26432903
samodur69 <69460645+samodur69@users.noreply.github.com>
2020-08-26 01:51:26 +0300

    card.s3db
    connected wt database.
    don't work Luhn

--- banking.py
+++ banking.py
@@ -1,10 +1,19 @@
 import random
 from string import digits
+import sqlite3 as sq
+
+
+class Cards:
+    id = 0
+    cards = []
+
+    def __init__(self):
+        pass
+
 
 class BankSystem:
-
-    prompt1 = '\n1. Create an acoount\n2. Log into account\n0. Exit>'
-    prompt2 = '\n1. Balance\n2. Log out\n0. Exit'
+    prompt1 = '\n1. Create an account\n2. Log into account\n0. Exit\n'
+    prompt2 = '\n1. Balance\n2. Log out\n0. Exit\n'
     prompt3 = '\nYou have successfully logged in!\n'
     prompt4 = 'Wrong card number or PIN!\n'
     prompt5 = '\nEnter your card number:\n'
@@ -15,12 +24,10 @@ class BankSystem:
     prompt10 = 'Wrong choice'
     prompt11 = '\nYou have successfully logged out!\n'
     IIN = '400000'
-#    seed_number = random.seed(random.randint(1,1000))
-    cards = []
-    card_info = {}
-    card_balance = {}
+    logged_card = None
 
-    def input(self, prompt):
+    @staticmethod
+    def input(prompt):
         action = input(prompt)
         return action
 
@@ -33,30 +40,26 @@ class BankSystem:
                 self.login()
             elif action == '0':
                 print('Bye!')
+                conn.close()
                 exit()
-            # TEST functions
-#            elif action == '69':
-#                print(self.card_info)
-#                print(self.card_balance)
-            #TEST functions
-
 
     def create_card(self):
-       card_number = self.generate_number()
-       card_pin = self.generate_pin()
-       print('{}\n{}\n{}\n{}\n{}'.format(self.prompt9, self.prompt7, card_number, self.prompt8, card_pin))
-       self.cards.append(card_number)
-       self.card_info[card_number] = card_pin
-       self.card_balance[card_number] = 0
+        card_number = self.generate_number()
+        card_pin = self.generate_pin()
+        user_id = random.randint(0, 1000000)
+        print('{}\n{}\n{}\n{}\n{}'.format(self.prompt9, self.prompt7, card_number, self.prompt8, card_pin))
+        cur.execute("""INSERT INTO card VALUES (?, ?, ?, ?)""", (user_id, card_number, card_pin, 0))
+        conn.commit()
 
     def generate_number(self):
         number = self.IIN
-        for i in range (9):
+        for i in range(9):
             number += random.choice(digits)
         number += self.checksum_generate(number)
         return number
 
-    def generate_pin(self):
+    @staticmethod
+    def generate_pin():
         pin = ''
         for i in range(4):
             pin += random.choice(digits)
@@ -65,57 +68,61 @@ class BankSystem:
     def login(self):
         card_number = self.input(self.prompt5)
         card_pin = self.input(self.prompt6)
-        if self.check_pin(card_number, card_pin) == True:
+        cur.execute("""SELECT 
+            number, 
+            pin 
+            FROM card 
+            WHERE number=:number""", {"number": card_number})
+        if card_pin in cur.fetchone():
+            self.logged_card = card_number
             print(self.prompt3)
             self.account_action()
         else:
             print(self.prompt4)
 
-    def check_pin(self, number, pin):
-        if number in self.card_info.keys():
-            if pin == self.card_info[number]:
-                return True
-            else:
-                return False
-        else:
-            return False
-
     def balance(self):
-        pass
+        cur.execute("""SELECT 
+            balance 
+            FROM card 
+            WHERE number=:number""", {"number": self.logged_card})
+        print(f'Balance: ', cur.fetchone())
 
-    def checksum_generate(self, number):
-        # number from string to integer
+    @staticmethod
+    def checksum_generate(number):
+        # make list of numbers from string 1char - 1 int
         dig_list = list(map(int, number))
         # multiply all odd elements by 2
-        print(dig_list)
         for i, item in enumerate(dig_list):
             if i % 2 == 0:
                 dig_list[i] *= 2
-        print(dig_list)
-        # substract 9 from numbers over 9
-        for i, item in enumerate(dig_list):
             if item > 9:
                 dig_list[i] -= 9
-        print(dig_list)
         # checksum + sum(dig_list) % 10 must be 0
-        print(sum(dig_list))
         checksum = 10 - (sum(dig_list) % 10)
-        print(checksum)
         return str(checksum)
 
     def account_action(self):
         print(self.prompt2)
         action = input()
         if action == '1':
-            pass
+            self.balance()
         elif action == '2':
+            self.logged_card = None
             print(self.prompt11)
-            return
         elif action == '0':
             print('\nBye!')
+            conn.close()
             exit()
-#        else:
-#            print(self.prompt10)
 
 
+conn = sq.connect('card.s3db')
+cur = conn.cursor()
+cur.execute("""CREATE TABLE IF NOT EXISTS card 
+                (
+                id INTEGER,
+                number TEXT,
+                pin TEXT,
+                balance INTEGER DEFAULT 0
+                )""")
+conn.commit()
 BankSystem().main_menu()
