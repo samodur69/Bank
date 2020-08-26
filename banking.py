a8b214d8a27909f9d62cd9700517ec03abe02f9b
samodur69 <69460645+samodur69@users.noreply.github.com>
2020-08-27 00:34:23 +0300

    Add files via upload
    add account actions

--- banking.py
+++ banking.py
@@ -12,17 +12,20 @@ class Cards:
 
 
 class BankSystem:
-    prompt1 = '\n1. Create an account\n2. Log into account\n0. Exit\n'
-    prompt2 = '\n1. Balance\n2. Log out\n0. Exit\n'
-    prompt3 = '\nYou have successfully logged in!\n'
+    prompt1 = '1. Create an account\n2. Log into account\n0. Exit\n'
+    prompt2 = '1. Balance\n2. Add income\n3. Do transfer\n4. Close account\n5. Log out\n0. Exit\n'
+    prompt3 = 'You have successfully logged in!\n'
     prompt4 = 'Wrong card number or PIN!\n'
-    prompt5 = '\nEnter your card number:\n'
+    prompt5 = 'Enter your card number:\n'
     prompt6 = 'Enter your PIN:\n'
     prompt7 = 'Your card number:>'
     prompt8 = 'Your card PIN:'
     prompt9 = 'Your card has been created'
     prompt10 = 'Wrong choice'
-    prompt11 = '\nYou have successfully logged out!\n'
+    prompt11 = 'You have successfully logged out!\n'
+    prompt12 = 'Enter income:\n'
+    prompt13 = 'Income was added!\n'
+    prompt14 = 'The account has been closed!\n'
     IIN = '400000'
     logged_card = None
 
@@ -30,6 +33,25 @@ class BankSystem:
     def input(prompt):
         action = input(prompt)
         return action
+
+    @staticmethod
+    def checksum_lhun(string):
+        digit = list(map(int, string))
+        odd_sum = sum(digit[-1::-2])
+        even_sum = sum([sum(divmod(2 * d, 10)) for d in digit[-2::-2]])
+        return (odd_sum + even_sum) % 10
+
+    def verify_card(self, number):
+        return self.checksum_lhun(number) == 0
+
+    def generate_lhun(self, string):
+        #  Generate the Luhn check digit to append to the provided string.
+        cksum = self.checksum_lhun(string + '0')
+        return str((10 - cksum) % 10)
+
+    def append_lhun(self, string):
+        #  Append Luhn check digit to the end of the provided string
+        return string + str(self.generate_lhun(string))
 
     def main_menu(self):
         while True:
@@ -55,7 +77,7 @@ class BankSystem:
         number = self.IIN
         for i in range(9):
             number += random.choice(digits)
-        number += self.checksum_generate(number)
+        number += self.generate_lhun(number)
         return number
 
     @staticmethod
@@ -68,12 +90,8 @@ class BankSystem:
     def login(self):
         card_number = self.input(self.prompt5)
         card_pin = self.input(self.prompt6)
-        cur.execute("""SELECT 
-            number, 
-            pin 
-            FROM card 
-            WHERE number=:number""", {"number": card_number})
-        if card_pin in cur.fetchone():
+        cur.execute("""SELECT number, pin FROM card WHERE number=:number""", {"number": card_number})
+        if (card_number, card_pin) == cur.fetchone():
             self.logged_card = card_number
             print(self.prompt3)
             self.account_action()
@@ -81,38 +99,83 @@ class BankSystem:
             print(self.prompt4)
 
     def balance(self):
-        cur.execute("""SELECT 
-            balance 
-            FROM card 
-            WHERE number=:number""", {"number": self.logged_card})
-        print(f'Balance: ', cur.fetchone())
+        cur.execute("""SELECT balance FROM card WHERE number=:number""", {"number": self.logged_card})
+        balance = cur.fetchone()
+        print(f'Balance: ', balance[0])
 
-    @staticmethod
-    def checksum_generate(number):
-        # make list of numbers from string 1char - 1 int
-        dig_list = list(map(int, number))
-        # multiply all odd elements by 2
-        for i, item in enumerate(dig_list):
-            if i % 2 == 0:
-                dig_list[i] *= 2
-            if item > 9:
-                dig_list[i] -= 9
-        # checksum + sum(dig_list) % 10 must be 0
-        checksum = 10 - (sum(dig_list) % 10)
-        return str(checksum)
+    def add_income(self):
+        income = int(self.input(self.prompt12))
+        cur.execute("""UPDATE card SET balance = balance +? WHERE number=?""", (income, self.logged_card))
+        conn.commit()
+        print(self.prompt13)
 
     def account_action(self):
-        print(self.prompt2)
-        action = input()
-        if action == '1':
-            self.balance()
-        elif action == '2':
-            self.logged_card = None
-            print(self.prompt11)
-        elif action == '0':
-            print('\nBye!')
-            conn.close()
-            exit()
+        while True:
+            print(self.prompt2)
+            action = input()
+            if action == '1':
+                self.balance()
+            elif action == '2':
+                self.add_income()
+            elif action == '3':
+                self.transfer_main()
+            elif action == '4':
+                self.close_account()
+                break
+            elif action == '5':
+                self.logged_card = None
+                print(self.prompt11)
+                break
+            elif action == '0':
+                print('\nBye!')
+                conn.close()
+                exit()
+
+    def close_account(self):
+        cur.execute("""DELETE FROM card 
+                        WHERE number=:number""", {"number": self.logged_card})
+        conn.commit()
+        self.logged_card = None
+        print(self.prompt14)
+
+    def transfer_main(self):
+        print('Transfer')
+        card_input = input('Enter card number:\n')
+        if self.check_transfer(card_input) is True:
+            amount = int(input('Enter how much money you want to transfer:\n'))
+            self.check_transfer_balance(amount)
+            self.make_transaction(card_input, amount)
+
+    def check_transfer(self, number):
+        if self.verify_card(number) is True:
+            if self.check_card_in_db(number) is True:
+                return True
+            else:
+                print('Such a card does not exist.\n')
+                return False
+        else:
+            print('Probably you made mistake in the card number. Please try again!\n')
+            return False
+
+    def check_transfer_balance(self, amount):
+        cur.execute("""SELECT balance FROM card WHERE number=:number""", {"number": self.logged_card})
+        balance = cur.fetchone()
+        if amount <= balance[0]:
+            return True
+        else:
+            print('Not enough money!\n')
+
+    @staticmethod
+    def check_card_in_db(number):
+        cur.execute("""SELECT number FROM card WHERE number=:number""", {"number": number})
+        if cur.fetchone() is not None:
+            return True
+
+    def make_transaction(self, number_to, amount):
+        cur.execute("""UPDATE card SET balance = balance +? WHERE number=?""", (amount, number_to))
+        cur.execute("""UPDATE card SET balance = balance -? WHERE number=?""", (amount, self.logged_card))
+        conn.commit()
+        print('Success!\n')
 
 
 conn = sq.connect('card.s3db')
@@ -124,5 +187,4 @@ cur.execute("""CREATE TABLE IF NOT EXISTS card
                 pin TEXT,
                 balance INTEGER DEFAULT 0
                 )""")
-conn.commit()
 BankSystem().main_menu()
